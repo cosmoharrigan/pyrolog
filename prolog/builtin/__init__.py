a7396855b1f9f7fb2357166fa1698ca7a7d5c7f9
Carl Friedrich Bolz <cfbolz@gmx.de>
2009-12-02 12:44:31 +0000

    migrate prolog interp

---
+++ prolog/builtin/__init__.py
@@ -0,0 +1,17 @@
+# all builtins
+builtins = {}
+builtins_list = []
+
+# imports to register builtins
+import prolog.builtin.allsolution
+import prolog.builtin.arithmeticbuiltin
+import prolog.builtin.atomconstruction
+import prolog.builtin.control
+import prolog.builtin.database
+import prolog.builtin.exception
+import prolog.builtin.formatting
+import prolog.builtin.metacall
+import prolog.builtin.parseraccess
+import prolog.builtin.source
+import prolog.builtin.termconstruction
+import prolog.builtin.unify
