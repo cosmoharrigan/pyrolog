913e3c89e3e9166c0c936455f7b6b08105ecaa55
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-02-25 11:59:55 +0000

    grr, left the debug print in

--- prolog/interpreter/translatedmain.py
+++ prolog/interpreter/translatedmain.py
@@ -21,7 +21,7 @@ class ContinueContinuation(Continuation):
         var_representation(self.var_to_pos, self.engine, self.write)
         while 1:
             res = getch()
-            self.write(repr(res)+"\n")
+            #self.write(repr(res)+"\n")
             if res in "\r\x04\n":
                 self.write("\n")
                 raise StopItNow()
