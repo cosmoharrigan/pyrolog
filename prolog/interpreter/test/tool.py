c14eeeb91f2477ec1a7fc0fd462e32f065a4d08f
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-01-03 22:17:00 +0100

    moved test_iso.py into the test folder next to the other test, also the inriasuite folder which contains the iso tests

--- prolog/interpreter/test/tool.py
+++ prolog/interpreter/test/tool.py
@@ -3,6 +3,7 @@ from prolog.interpreter.error import UnificationFailed
 from prolog.interpreter.parsing import parse_query_term, get_engine
 from prolog.interpreter.continuation import Continuation, Heap, Engine
 from prolog.interpreter.parsing import parse_file, TermBuilder
+
 def assert_true(query, e=None):
     if e is None:
         e = Engine()
@@ -11,6 +12,7 @@ def assert_true(query, e=None):
     e.run(term)
     return dict([(name, var.dereference(e.heap))
                      for name, var in vars.iteritems()])
+
 def assert_false(query, e=None):
     if e is None:
         e = Engine()
