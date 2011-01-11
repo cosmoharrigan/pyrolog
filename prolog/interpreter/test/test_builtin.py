f228ab1f478adf5300713186f6ead375fa74b573
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-01-11 14:35:36 +0100

    Added new test for number_chars.

--- prolog/interpreter/test/test_builtin.py
+++ prolog/interpreter/test/test_builtin.py
@@ -558,8 +558,9 @@ def test_write_term():
 def test_number_chars():
     assert_true("number_chars(123, ['1', '2', '3']).")
     assert_true("number_chars(123, X), X = ['1', '2', '3'].")
+    prolog_raises("syntax_error(list)", "number_chars(X, a)")
     assert_true("number_chars(X, ['1', '2', '3']), X = 123.")
-    assert_true("number_chars(-123, ['-', '1', '2', '3']).")
+    assert_true("number_chars(-123, [-, '1', '2', '3']).")
     assert_true("number_chars(123.1, ['1', '2', '3', '.', '1']).")
     assert_true("number_chars(1000000000000000, ['1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']).")
     prolog_raises("instantiation_error", "number_chars(X, Y)")
