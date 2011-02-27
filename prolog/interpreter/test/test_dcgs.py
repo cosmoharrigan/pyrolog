b7b869ca9663273f5aebd1e12546ede7ea654e09
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-02-27 23:26:26 +0100

    Added tests for dcg module, some tests don't pass because of a strange bug i couldn't fix yet.
    Added also a list module, because the dcg prolog code needs some
    basic list predicates.

--- prolog/interpreter/test/test_dcgs.py
+++ prolog/interpreter/test/test_dcgs.py
@@ -1,3 +1,56 @@
 import py
+from prolog.interpreter.test.tool import assert_true, get_engine
+from prolog.interpreter.continuation import Engine
 
+loc = "../../prolog_modules/"
+e = get_engine("""
+:- use_module('%s%s').
+""" % (loc, "dcg"))
 
+def test_a_to_b():
+    assert_true("""
+    trans((a --> b), X),
+    X = (a(X1, X2) :- b(X1, X2)).
+    """, e)
+
+def test_a_to_b_c():
+    assert_true("""
+    trans((a --> b, c), X),
+    X = (a(X1, X3) :- b(X1, X2), c(X2, X3)).
+    """, e)
+
+def test_single_terminal():
+    assert_true("""
+    trans((a --> [b]), X),
+    X = (a([b|X1], X1) :- true).
+    """, e)
+
+def test_terminal_nonterminal():
+    assert_true("""
+    trans((a --> [c], b), X),
+    X = (a([c|Y1], Y2) :- b(Y1, Y2)).
+    """, e)
+
+def test_nonterminal_terminal():
+    assert_true("""
+    trans((a --> b, [c]), X),
+    X = (a(X1, X3) :- b(X1, X2), X2 = [c|X3]).
+    """, e)
+
+def test_terminal_terminal():
+    assert_true("""
+    trans((a --> [b], [c]), X),
+    X = (a([b|X1], X2) :- X1 = [c|X2]).
+    """, e)
+
+def test_nonterminal_param_1():
+    assert_true("""
+    trans((a(X) --> b), R),
+    R = (a(X, X1, X2) :- b(X1, X2)).
+    """, e)
+
+def test_nonterminal_param_2():
+    assert_true("""
+    trans((a(X, Y) --> b(Y)), R),
+    R = (a(X, Y, X1, X2) :- b(Y, X1, X2)).
+    """, e)
--- prolog/prolog_modules/dcg.pl
+++ prolog/prolog_modules/dcg.pl
@@ -1,34 +1,49 @@
+:- module(dcg, [trans/2]).
+:- use_module('../../prolog_modules/list').
+
 trans((H --> B), (TransH :- TransB)) :-
 	trans_head(H, X0, X1, TransH),
 	trans_body(B, X0, X1, TransB).
 
 trans_head(H, X0, X1, F) :-
 	atom(H), !,
-	functor(F, H, 2),
-	arg(1, F, X0),
-	arg(2, F, X1).
+	F =.. [H, X0, X1].
 
 trans_head(H, X0, X1, F) :-
 	H =.. List, !,
 	append(List, [X0], Args1),
 	append(Args1, [X1], Args2),
-	F =.. Args2.
+	F =.. Args2. 
+
+trans_body((B1, B2), X0, XE, XTemp = R) :-
+	is_list(B1),
+	is_list(B2), !,
+	append(B1, XTemp, X0),
+	append(B2, XE, R).
+
+trans_body((B1, B2), X0, XE, F2) :-
+	is_list(B1), !,
+	append(B1, XTemp, X0),
+	trans_body(B2, XTemp, XE, F2).
+
+trans_body((B1, B2), X0, XE, (F1, XTemp = R)) :-
+	is_list(B2), !,
+	trans_body(B1, X0, XTemp, F1),
+	append(B2, XE, R).
 
 trans_body((B1, B2), X0, XE, (F1, F2)) :-
 	trans_body(B1, X0, XTemp, F1),
 	trans_body(B2, XTemp, XE, F2), !.
 
 trans_body(B, X0, XE, F) :-
-	atom(B),
-	functor(F, B, 2),
-	arg(1, F, X0),
-	arg(2, F, XE), !.
+	atom(B), !,
+	F =.. [B, X0, XE].
 
 trans_body(B, X0, XE, true) :-
 	is_list(B),
-	prepend(B, XE, R),
+	append(B, XE, R),
 	X0 = R, !.
-
+/*
 trans_body(B, X0, XE, F) :-
 	B =.. [Functor|Args],
 	Functor \= {}, !,
@@ -40,11 +55,4 @@ trans_body({C1, C2}, X0, XE, (C1, R)) :-
 	trans_body({C2}, X0, XE, R), !.
 
 trans_body({C}, X0, XE, (C, X0=XE)) :- !.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-prepend([], L, L).
-prepend([H|T], L, [H|R]) :-
-	prepend(T, L, R).
-
-	
+*/
