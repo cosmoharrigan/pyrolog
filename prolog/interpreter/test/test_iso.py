53dcca9eb7e53927a2ee116ff907b0e961102c6f
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-03-02 00:10:03 +0100

    Augmented test_iso with xfail capability, merged different tests into one test.

--- prolog/interpreter/test/test_iso.py
+++ prolog/interpreter/test/test_iso.py
@@ -11,6 +11,8 @@ TESTDIR = str(py.path.local(__file__).dirpath().join('inriasuite'))
 
 FAILURE = 'failure'
 SUCCESS = 'success'
+SKIP = "_SKIP_"
+XFAIL = "_XFAIL_"
 
 def deconstruct_line(line):
     HALT = 'halt,'
@@ -57,15 +59,16 @@ def deconstruct_line(line):
 def get_lines(file_):
     testfile = open(TESTDIR + '/' + file_)
     for test in testfile.readlines():
-        if test.endswith('%%SKIP%%\n') or test.find('0\'') != -1:
-            continue
-        if test.startswith('['):
+        if test.endswith('%%SKIP%%\n'):
+            yield SKIP, ""
+        elif test.find('0\'') != -1 or test.find("current_prolog_flag") != -1:
+            yield XFAIL, ""
+        elif test.startswith('['):
             last_bracket = test.rfind(']')
             first_bracket = test.find('[') + 1
             relevant = test[first_bracket:last_bracket]
             left, right = deconstruct_line(relevant)
             yield left, right
-
 
 def deconstruct_list(l):
     pieces = [piece for piece in l.split('], [')]
@@ -78,60 +81,59 @@ def get_files():
     for file in content:
         yield file
 
-
 def pytest_generate_tests(metafunc):
-    names = metafunc.funcargnames
     for f in get_files():
         for left, right in get_lines(f):
+            # these tests can't pass because not implemented functionality is 
+            # required, or they don't make sense at all...
+            if left in [SKIP, XFAIL]:
+                metafunc.addcall(funcargs = dict(cmd = "skip", test = "", param = left))
             # simple test: failure or success
             if right in (FAILURE, SUCCESS):
-                if 'mode' in names:
-                    metafunc.addcall(funcargs = dict(test = (left + '.'), mode = right))
+                metafunc.addcall(funcargs=dict(cmd="simple", test=(left + '.'), param=right))
             # test whether an error occurs
             if right.find('error') != -1:
-                if 'error' in names:
-                    metafunc.addcall(funcargs = dict(test = left, error = right))
+                metafunc.addcall(funcargs=dict(cmd="error", test=left, param=right))
             # test unification with a list of unifications
             if right.find('[') != -1 and right.find('error') == -1:
                 lists = deconstruct_list(right[1:-2])
-                if 'lists' in names:
-                    metafunc.addcall(funcargs = dict(test = left, lists = lists))
+                metafunc.addcall(funcargs=dict(cmd="list", test=left, param=lists))
 
-
-def test_error(test, error):
-    try:
-        prolog_raises(error, test)
-    except UncaughtError, e:
-        msg = repr(e.term)
-        if 'existence_error' in msg or 'type_error' in msg:
-            py.test.skip(msg)
-        else:
-            raise
-
-
-def test_success_failure(test, mode):
-    try:
-        if mode == FAILURE:
-            assert_false(test)
-        elif mode == SUCCESS:
-            assert_true(test)
-    except (error.UncaughtError, error.CatchableError), e:
-        msg = repr(e.term)
-        if 'existence_error' in msg:
-            py.test.skip(msg)
-        else:
-            raise
-
-
-def test_multiple_lists(test, lists):
-    try:
-        for goal in lists:
-            check = test + ', ' + goal.replace('<--', '=') + '.'
-            assert_true(check)
-    except (error.UncaughtError, error.CatchableError), e:
-        msg = repr(e.term)
-        if 'existence_error' in msg:
-            py.test.skip(msg)
-        else:
-            raise
-        
+def test_all_tests(cmd, test, param):
+    if cmd == "skip":
+        if param == SKIP:
+            py.test.skip("")
+        elif param == XFAIL:
+            py.test.xfail("")
+    elif cmd == "simple":
+        try:
+            if param == FAILURE:
+                assert_false(test)
+            elif param == SUCCESS:
+                assert_true(test)
+        except (error.UncaughtError, error.CatchableError), e:
+            msg = repr(e.term)
+            if 'existence_error' in msg:
+                py.test.skip(msg)
+            else:
+                raise
+    elif cmd == "error":
+        try:
+            prolog_raises(param, test)
+        except UncaughtError, e:
+            msg = repr(e.term)
+            if 'existence_error' in msg or 'type_error' in msg:
+                py.test.skip(msg)
+            else:
+                raise
+    elif cmd == "list":
+        try:
+            for goal in param:
+                check = test + ', ' + goal.replace('<--', '=') + '.'
+                assert_true(check)
+        except (error.UncaughtError, error.CatchableError), e:
+            msg = repr(e.term)
+            if 'existence_error' in msg:
+                py.test.skip(msg)
+            else:
+                raise
