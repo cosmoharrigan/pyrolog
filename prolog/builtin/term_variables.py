b919e4875529fc70cd3c1ae57629c5e6a82cbb7f
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-03-10 17:37:31 +0100

    (cfbolz, seigoryu, hager) Fixed bug in term_variables.

--- prolog/builtin/term_variables.py
+++ prolog/builtin/term_variables.py
@@ -5,17 +5,18 @@ from prolog.interpreter.helper import wrap_list, is_term
 
 @expose_builtin("term_variables", unwrap_spec=["obj", "obj"])
 def impl_term_variables(engine, heap, prolog_term, variables):
-    varlist, _ = find_variables(prolog_term, [], {})
-    wrap_list(varlist).unify(variables, heap)
+    varlist = []
+    seen = {}
+    todo = [prolog_term]
+    while todo:
+        prolog_term = todo.pop().dereference(None)
+        if isinstance(prolog_term, term.Var):
+            if prolog_term not in seen:
+                varlist.append(prolog_term)
+                seen[prolog_term] = None
+        elif is_term(prolog_term):
+            numargs = prolog_term.argument_count()
+            for i in range(numargs - 1, -1, -1):
+                todo.append(prolog_term.argument_at(i))
 
-def find_variables(prolog_term, variables, vardict):
-    if isinstance(prolog_term, term.Var) and not prolog_term in vardict:
-        variables.append(prolog_term)
-        vardict[prolog_term] = True
-    elif is_term(prolog_term):
-        numargs = prolog_term.argument_count()
-        for i in range(numargs):
-            variables, vardict = find_variables(prolog_term.argument_at(i),
-                    variables, vardict)
-    # else return parameters
-    return variables, vardict
+    wrap_list(varlist).unify(variables, heap)
--- prolog/interpreter/test/test_term_variables.py
+++ prolog/interpreter/test/test_term_variables.py
@@ -1,5 +1,6 @@
 import py
 from prolog.interpreter.test.tool import assert_true, assert_false, prolog_raises
+from prolog.interpreter.test.tool import get_engine
 
 def test_basic_term_variables():
     assert_true("term_variables(X, [X]).")
@@ -14,10 +15,19 @@ def test_more_advanced_term_variables():
     assert_true("term_variables([X, Y, a, f(g(A), X)], [X, Y, A]).")
     assert_true("term_variables((A :- B, C, A), [A,B,C]).")
     assert_true("term_variables(f(X, f(X)), [X]).")
-    assert_true("X = 1, term_variables(f(X, Y), L), Y = 2.")
+    assert_true("X = 1, term_variables(f(X, Y), L), L == [Y], Y = 2.")
+    assert_true("X = Y, term_variables(f(X, Y), L), L == [Y], Y = 2.")
 
 def test_var_binding():
     assert_true("X = a, term_variables(X, []).")
     assert_true("term_variables(X, L), X = a, L = [a].")
     assert_true("X = f(A,B), term_variables(X, [A,B]).")
 
+def test_term_variables_huge_list():
+    e = get_engine("""
+        make_triple_list(0, _, []).
+        make_triple_list(X, Y, [Y, Y, Y | T]) :-
+            X > 0, X1 is X - 1,
+            make_triple_list(X1, Y, T).
+            """)
+    assert_true("make_triple_list(4000, a, L), term_variables(L, L1), L1 == [].", e)
