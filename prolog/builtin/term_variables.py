1b8ab8662e2850232dce36920cfbb258c55137af
Sven Hager <sven.hager@uni-duesseldorf.de>
2011-03-22 17:26:46 +0100

    Refactored code for term_variables and term_attvars.

--- prolog/builtin/term_variables.py
+++ prolog/builtin/term_variables.py
@@ -1,20 +1,19 @@
 import py
+from pypy.rlib.objectmodel import specialize
 from prolog.builtin.register import expose_builtin
 from prolog.interpreter import term
 from prolog.interpreter.helper import wrap_list, is_term
 from prolog.interpreter.memo import CopyMemo
 from prolog.interpreter.term import Var, AttVar, Callable
-
 from prolog.interpreter.error import UnificationFailed
 
 @expose_builtin("term_variables", unwrap_spec=["obj", "obj"])
 def impl_term_variables(engine, heap, prolog_term, variables):
     term_variables(engine, heap, prolog_term, variables)
 
-
+@specialize.arg(4)
 def term_variables(engine, heap, prolog_term, variables, consider_attributes=False):
     varlist = []
-    #if consider_attributes:
     varc = variables
     seen = {}
     todo = [prolog_term]
@@ -28,10 +27,9 @@ def term_variables(engine, heap, prolog_term, variables, consider_attributes=Fal
             if consider_attributes and not value.atts:
                 continue
             if value not in seen:
-                varlist.append(value.dereference(heap))
+                varlist.append(value)
                 seen[value] = None
-                #if consider_attributes:
-                X = Var()
+                X = heap.newvar()
                 prolog_list = Callable.build(".", [value, X])
                 prolog_list.unify(varc, heap)
                 varc = X
@@ -39,4 +37,4 @@ def term_variables(engine, heap, prolog_term, variables, consider_attributes=Fal
             numargs = value.argument_count()
             for i in range(numargs - 1, -1, -1):
                 todo.append(value.argument_at(i))
-    wrap_list(varlist).unify(variables, heap)
+    varc.unify(Callable.build("[]"), heap)
